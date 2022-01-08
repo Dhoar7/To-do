95d4180d8d3541781a48461e5c551454df36925c
Arun Kumar <marun3400@gmail.com>
2022-01-08 19:27:58 +0530

    Updates

---
+++ precache-manifest.dab5babe6b6e23e608288ee1b18dad09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa42982eb259001ad719bd4076d0ffd0",
+    "url": "/To-do/index.html"
+  },
+  {
+    "revision": "86c6acba774c8d55597a",
+    "url": "/To-do/static/css/main.507dda69.chunk.css"
+  },
+  {
+    "revision": "ad097d7b9296ffd18e33",
+    "url": "/To-do/static/js/2.0bb6c45d.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/To-do/static/js/2.0bb6c45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c6acba774c8d55597a",
+    "url": "/To-do/static/js/main.1cf98916.chunk.js"
+  },
+  {
+    "revision": "fd8df061f95aec98b24c",
+    "url": "/To-do/static/js/runtime-main.0709d376.js"
+  }
+]);
